a457dead928db5ec393467553b0eaeb0c715db8c
renasami <drrr0502@gmail.com>
2021-09-24 18:26:04 +0900

    大体メイン画面終了

--- react_native/screens/Main.tsx
+++ react_native/screens/Main.tsx
@@ -1,18 +1,39 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView,SafeAreaView,Button } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function Main({ navigation }: RootTabScreenProps<'Main'>) {
+
+  const userName = () => {
+    return "sansan"
+  }
+  const name = "sansan"
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <Text>hello expo world!</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.hello}>こんにちは{name}</Text>
+        <View style={styles.notify} lightColor="#eee" darkColor="rgba(255,255,255,0.1)">
+          <Text>ここに通知が来るンゴ</Text>
+        </View>
+        <View style={styles.notify} lightColor="#eee" darkColor="rgba(255,255,255,0.1)">
+          <Text>ここはグラフ</Text>
+        </View>
+      </ScrollView>
+      <View　style={{ flexDirection: 'row'}}>
+        <Button 
+          title="Press me"
+          onPress={() => alert('Simple Button pressed')}
+        />
+        <Button 
+          title="Press me2"
+          onPress={() => alert('Simple Button pressed')}
+        />
+      </View>
+    </SafeAreaView> 
   );
 }
 
@@ -31,4 +52,29 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  hello:{
+    fontSize:10,
+  },
+  scrollView: {
+    width: '100%',
+    textAlign: 'center',
+    marginHorizontal: 20,
+  },
+  notify:{
+  height: 188,
+  top: 50,
+  marginTop:20,
+  width: '80%',
+  left:"10%",
+  textAlign: 'center',
+  borderRadius: 22,
+  borderStyle:"solid",
+  borderWidth:1
+  },
+  graph:{
+    width: '80%',
+    height: '40%',
+    borderStyle:"solid",
+    borderWidth:1
+  }
 });
