ae3b7ae0a0f510bb988c40306c3373a78555fd61
Daiki Suzuki <suzuki11235813@gmail.com>
2021-09-24 19:05:34 +0900

    Revert "Feature/deploy test"

--- react_native/App.tsx
+++ react_native/App.tsx
@@ -1,17 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { StyleSheet, Text, View } from 'react-native';
+
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-import Amplify from '@aws-amplify/core';
-import config from './src/aws-exports';
-import {withAuthenticator} from 'aws-amplify-react-native';
-Amplify.configure(config);
-
-function App() {
+export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -26,13 +21,3 @@ function App() {
     );
   }
 }
-
-export default withAuthenticator(App, {includeGreetings: true});
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
