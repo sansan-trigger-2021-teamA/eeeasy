3da4d6e3786879728d2567e7240ecc45fbc7df50
renasami <drrr0502@gmail.com>
2021-09-24 17:45:46 +0900

    画面遷移終了

--- react_native/types.tsx
+++ react_native/types.tsx
@@ -25,8 +25,11 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 >;
 
 export type RootTabParamList = {
-  TabOne: undefined;
+  Main: undefined;
   TabTwo: undefined;
+  WaitingCompany: undefined;
+  Coupon: undefined;
+  Profile: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
