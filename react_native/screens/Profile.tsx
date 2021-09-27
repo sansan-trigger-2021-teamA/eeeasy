05d953b36dd05218a4d361cdcd59c4f351958e86
renasami <drrr0502@gmail.com>
2021-09-27 15:24:51 +0900

    fin profile

--- react_native/screens/Profile.tsx
+++ react_native/screens/Profile.tsx
@@ -1,15 +1,30 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet,Button } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+//職業
 export default function Profile() {
+    const profile = {
+        name:"茂野　吾郎",
+        sex:"男",
+        age:"21",
+        job:"学生"
+    }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>確認&編集</Text> 
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <Text style={styles.infoText}>name:{profile.name}</Text> 
+      <Text style={styles.infoText}>sex:{profile.sex}</Text>     
+      <Text style={styles.infoText}>age:{profile.age}</Text> 
+      <Text style={styles.infoText}>job:{profile.job}</Text> 
+      <Button 
+      title="職業を変更する"
+      onPress={() => alert('職業変更未実装')}
+      />
     </View>
   );
 }
@@ -18,10 +33,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:"5%"
+  },
+  button:{
+    marginTop:"10%"
+  },
+  infoText: {
+    paddingVertical:"5%",
+    fontSize:20
   },
   title: {
-    fontSize: 20,
+    fontSize: 25,
     fontWeight: 'bold',
   },
   separator: {
