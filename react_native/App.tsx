710eed14544d23305de9a974f694e5add249a2b2
renasami <drrr0502@gmail.com>
2021-09-29 13:10:04 +0900

    backend gps fin

--- react_native/App.tsx
+++ react_native/App.tsx
@@ -12,6 +12,7 @@ import {withAuthenticator} from 'aws-amplify-react-native';
 //import { registerTask } from './controllers/backendGeoLocation'
 import * as BackgroundFetch from "expo-background-fetch"
 import * as TaskManager from "expo-task-manager"
+import * as Location from 'expo-location';
 
 Amplify.configure({
   ...awsmobile,
@@ -19,6 +20,23 @@ Amplify.configure({
   disabled: true,
 },});
 
+const LOCATION_TASK_NAME = 'background-location-task';
+
+TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
+  if (error) {
+    // Error occurred - check `error.message` for more details.
+    console.log(error)
+    return;
+  }
+  if (data) {
+    console.log(data);
+    // do something with the locations captured in the background
+  }
+});
+
+Location.startLocationUpdatesAsync(LOCATION_TASK_NAME,{
+  timeInterval:1000,
+})
 
 function App() {
   const isLoadingComplete = useCachedResources();
