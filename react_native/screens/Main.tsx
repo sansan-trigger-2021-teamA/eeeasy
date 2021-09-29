7ae94acfc9423f6c9fefcb7afe1691b4f81be315
ryonasso <ryounaoyuki@gmail.com>
2021-09-30 08:39:31 +0900

    feat : 論文の還元を追加

--- react_native/screens/Main.tsx
+++ react_native/screens/Main.tsx
@@ -1,80 +1,89 @@
 import * as React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
-import  AsyncStorage  from "@react-native-async-storage/async-storage"
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
 import { UserContext } from "../context/UserContext";
 import { Button } from "react-native-material-ui";
 import * as Location from "expo-location";
-import { useEffect, useRef,useState } from 'react';
-import * as Notifications from 'expo-notifications';
-import Constants from 'expo-constants';
-import { Subscription } from '@unimodules/core';
+import { useEffect, useRef, useState } from "react";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+import { Subscription } from "@unimodules/core";
 import { RewardGraph } from "../components/RewardGraph";
 import { DistanceGraph } from "../components/DistanceGraph";
 
-  
 export default function Main({ navigation }: RootTabScreenProps<"Main">) {
   const notificationListener = useRef<Subscription>();
   const responseListener = useRef<Subscription>();
-  const [expoPushToken, setExpoPushToken] = useState('');
-  const [notification, setNotification] = useState<Notifications.Notification>();
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState<
+    Notifications.Notification
+  >();
   useEffect(() => {
     // 通知を受信した時の振る舞いを設定
     let registerForPushNotificationsAsync = async () => {
       if (Constants.isDevice) {
-        const { status: existingStatus } = await Notifications.getPermissionsAsync()
-        let finalStatus = existingStatus
-        if (existingStatus !== 'granted') {
+        const {
+          status: existingStatus,
+        } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== "granted") {
           // 通知の権限がない場合は、再度通知の権限を確認します
-          const { status } = await Notifications.requestPermissionsAsync()
+          const { status } = await Notifications.requestPermissionsAsync();
           finalStatus = status;
         }
-        if (finalStatus !== 'granted') {
-          alert('Failed to get push token for push notification!')
+        if (finalStatus !== "granted") {
+          alert("Failed to get push token for push notification!");
         } else {
-          // Pushトークンの取得 
+          // Pushトークンの取得
           const token = (await Notifications.getExpoPushTokenAsync()).data;
           console.log(token);
           return token;
         }
       }
-      
     };
-    registerForPushNotificationsAsync().then(token => token && setExpoPushToken(token))
+    registerForPushNotificationsAsync().then(
+      (token) => token && setExpoPushToken(token)
+    );
 
     // アプリがフォアグラウンドの状態で通知を受信したときに起動
-    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-      setNotification(notification)
-    })
+    notificationListener.current = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        setNotification(notification);
+      }
+    );
 
     // ユーザーが通知をタップまたは操作したときに発生します
     // （アプリがフォアグラウンド、バックグラウンド、またはキルされたときに動作します）
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      alert('ユーザーが通知をタップしました')
-      console.log(response)
-    })
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        alert("ユーザーが通知をタップしました");
+        console.log(response);
+      }
+    );
 
     // アンマウント時にリスナーを削除
     return () => {
-      const notification = notificationListener.current
-      notification && Notifications.removeNotificationSubscription(notification)
-      const response = responseListener.current
-      response && Notifications.removeNotificationSubscription(response)
-    }
+      const notification = notificationListener.current;
+      notification &&
+        Notifications.removeNotificationSubscription(notification);
+      const response = responseListener.current;
+      response && Notifications.removeNotificationSubscription(response);
+    };
   }, []);
   const context = React.useContext(UserContext);
   const [graph, setGraph] = React.useState(false);
 
   React.useEffect(() => {
     AsyncStorage.getItem("User").then((user) => {
-      if(user){
-        context.setUser(JSON.parse(user))
-        return
+      if (user) {
+        context.setUser(JSON.parse(user));
+        return;
       }
       navigation.navigate("Modal");
-    })
+    });
   }, []);
 
   useEffect(() => {
@@ -103,7 +112,33 @@ export default function Main({ navigation }: RootTabScreenProps<"Main">) {
           lightColor="#f5f5f5"
           darkColor="rgba(255,255,255,0.1)"
         >
-          <Text>ここに通知が来るンゴ</Text>
+          <View lightColor="#f5f5f5" style={{ marginVertical: 3 }}>
+            <Text style={{ fontSize: 12 }}>
+              "ビッグデータ を活用する確率モデリング技術" : 本村陽一
+            </Text>
+          </View>
+          <View lightColor="#f5f5f5" style={{ marginVertical: 3 }}>
+            <Text style={{ fontSize: 12 }}>
+              "パナソニックの「脱プロダクトアウト」に向けた共創型プロジェクト" :
+              宮下雄治, 端義幸
+            </Text>
+          </View>
+          <View lightColor="#f5f5f5" style={{ marginVertical: 3 }}>
+            <Text style={{ fontSize: 12 }}>
+              "教育ビッグデータの利活用に向けた学習ログの蓄積と分析" : 緒方広明
+            </Text>
+          </View>
+          <View lightColor="#f5f5f5" style={{ marginVertical: 3 }}>
+            <Text style={{ fontSize: 12 }}>
+              "医療ビッグデータの医療政策への活用" : 松田晋哉
+            </Text>
+          </View>
+          <View lightColor="#f5f5f5" style={{ marginVertical: 3 }}>
+            <Text style={{ fontSize: 12 }}>
+              "情報流通プラットフォーム構築・運用プロジェクトのセキュリティマネジメント"
+              : 伊東明
+            </Text>
+          </View>
         </View>
         <View
           style={styles.graphArea}
@@ -155,7 +190,8 @@ const styles = StyleSheet.create({
     textAlign: "center",
     borderRadius: 22,
     borderStyle: "solid",
-    borderWidth: 1,
+    // borderWidth: 1,
+    padding: 10,
   },
   graphArea: {
     height: 188,
